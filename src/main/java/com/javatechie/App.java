51e4ed058d32eae144f7ab548f3900018e0f9e8a
Jogeshwar Ghosh <jogesh.san@gmail.com>
2025-05-30 12:47:57 +0530

    added if condition

--- src/main/java/com/javatechie/App.java
+++ src/main/java/com/javatechie/App.java
@@ -7,11 +7,14 @@ package com.javatechie;
 public class App 
 {
     public void addData(String data) {
-        System.out.println("data = "+data);
+        //System.out.println("data = "+data);
+        if (data.equals("java techie")) {
+            //System.out.println("welcome");
+        }
     }
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
     }
 }
